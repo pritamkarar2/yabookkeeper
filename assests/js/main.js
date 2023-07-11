6e126e3b8fa4ac70fa3ef1ec8d506a04ecaaea06
Chandanshaw004 <chandan@yourflow.com.au>
2023-07-11 13:31:51 +0530

    About Service Solutions pages are done

--- assests/js/main.js
+++ assests/js/main.js
@@ -1,36 +1,28 @@
+
+let mybutton = document.getElementById("myBtn");
+
 // Sticky Header
-window.onscroll = function() {myFunction()};
+window.onscroll = function() {onScroll()};
 
 var header = document.getElementById("header");
 var sticky = header.offsetTop;
 
-function myFunction() {
+function onScroll() {
   if (window.pageYOffset > sticky) {
     header.classList.add("stickyHeader");
   } else {
     header.classList.remove("stickyHeader");
   }
+
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
 }
 
-// Back to top 
-// window.onscroll = function() {scrollToTop()};
-
-// var body = document.getElementsByName('body')
-
-// function scrollToTop() {
-//   if(window.scrollTop > 200){
-//     body.classList.add("go-top  ")
-//   }
-//   //Scroll event
-//   $(window).scroll(function(){
-//     var scrolled = $(window).scrollTop();
-//     if (scrolled > 200) $('.go-top').fadeIn('slow');
-//     if (scrolled < 200) $('.go-top').fadeOut('slow');
-//   });
-  
-//   //Click event
-//   $('.go-top').click(function () {
-//     $("html, body").animate({ scrollTop: "0" },  500);
-//   });
-
-// });
+// When the user clicks on the button, scroll to the top of the document
+function backtoTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
