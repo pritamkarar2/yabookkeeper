93e8e4e4f07ed1c42407b62806469b47d860dd4f
vicky-veridoc <pritam@veridocglobal.com>
2023-07-13 12:48:20 +0530

    Minor fix

--- assests/js/page-links.js
+++ assests/js/page-links.js
@@ -6,12 +6,11 @@ var pages = {
     "contact": "Contact Us"
 };
 
-var host = new URL(window.location.protocol + "//" +  window.location.host);
+var host = window.location;
 console.log(host.pathname);
 
 var isProduction = (host.hostname != '127.0.0.1' && host.hostname != 'localhost' && host.hostname != '0.0.0.0');
 var page = '';
-
 $.each(pages, function (key, val) {
     var isActive = (host.pathname == ('/' + key));
     page += `<li class="nav-item"><a class="nav-link` + (isActive ? ' active" aria-current="page"' : '"') + ` href="./` + key + ((isProduction || val == "Home") ? '' : '.html') + `">` + val + `</a></li>`;
