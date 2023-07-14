4bfbdf3557e5ceba06d4c0c147e4d6766d5f9c4d
vicky-veridoc <pritam@veridocglobal.com>
2023-07-14 15:25:12 +0530

    Page link js structure change and implementing meta js

---
+++ assets/js/manage-meta.js
@@ -0,0 +1,15 @@
+const metadata = {
+    "description": "Manage Your Accounting Needs with Your Actual Bookkeeper",
+    "keyword": "yabookkeeper, bookkeepers"
+};
+
+const host = window.location; const hostname = host.hostname;
+const isProduction = (hostname != '127.0.0.1' && hostname != 'localhost' && hostname != '0.0.0.0');
+if (!isProduction) {
+    Object.keys(metadata).map(function (name) {
+        var meta = document.createElement('meta');
+        meta.name = name;
+        meta.content = metadata[name];
+        document.getElementsByTagName('head')[0].appendChild(meta);
+    });
+}
--- assets/js/page-links.js
+++ assets/js/page-links.js
@@ -6,11 +6,15 @@ const pageList = {
     "contact": "Contact Us"
 };
 
-const host = window.location; const hostname = host.hostname;
-const isProduction = (hostname != '127.0.0.1' && hostname != 'localhost' && hostname != '0.0.0.0');
-let pages = '';
-$.each(pageList, function (slug, name) {
-    const isActive = (host.pathname == ('/' + slug));
-    pages += `<li class="nav-item"><a class="nav-link` + (isActive ? ' active" aria-current="page"' : '"') + ` href="./` + slug + ((isProduction || name == "Home") ? '' : '.html') + `">` + name + `</a></li>`;
-});
-document.getElementById("navbarNav").innerHTML = `<ul class="navbar-nav ms-auto">` + pages + `</ul>`;
+function setPageList() {
+    const host = window.location; const hostname = host.hostname;
+    const isProduction = (hostname != '127.0.0.1' && hostname != 'localhost' && hostname != '0.0.0.0');
+    let pages = '';
+    Object.keys(pageList).map(function (slug) {
+        const isActive = (host.pathname == ('/' + slug));
+        pages += `<li class="nav-item"><a class="nav-link` + (isActive ? ' active" aria-current="page"' : '"') + ` href="./` + slug + ((isProduction || pageList[slug] == "Home") ? '' : '.html') + `">` + pageList[slug] + `</a></li>`;
+    });
+    document.getElementById("navbarNav").innerHTML = `<ul class="navbar-nav ms-auto">` + pages + `</ul>`;
+}
+
+window.addEventListener("load", setPageList);
